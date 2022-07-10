cb83fb932ca740f4d8321500ed8e3c8b1f1aa3ee
TeggunDrut <Acto7e@gmail.com>
2022-07-10 14:39:02 -0400

    ooga booga minesweeper

---
+++ src/main.rs
@@ -0,0 +1,29 @@
+use bevy::prelude::*;
+use board_plugin::BoardPlugin;
+
+#[cfg(feature = "debug")]
+use bevy_inspector_egui::WorldInspectorPlugin;
+
+fn main() {
+    let mut app = App::new();
+    app.insert_resource(WindowDescriptor {
+        title: "Mine Sweeper!".to_string(),
+        width: 700.,
+        height: 800.,
+        ..Default::default()
+    })
+    .add_plugins(DefaultPlugins);
+    #[cfg(feature = "debug")]
+    app.add_plugin(BoardPlugin);
+    app.add_plugin(WorldInspectorPlugin::new());
+
+    app.add_startup_system(camera_setup);
+    app.run();
+}
+
+fn camera_setup(mut commands: Commands) {
+    // 2D orthographic camera
+    commands.spawn_bundle(OrthographicCameraBundle::new_2d());
+}
+
+// ..
---
+++ src/math.rs
@@ -0,0 +1,7 @@
+
+pub mod math {
+  pub fn add(num1: i32, num2: i32) -> i32 {
+    let num3: i32 = num1 + num2;
+    return num3;
+  }
+}
